5621bae015d083872fab7db8ca03f0e91ecf5d2d
Aquilino Blanco <aqblanco@gmail.com>
2019-08-23 15:23:11 +0200

    Parsing function moved to its own function. Added export for list function.

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function list(pathToSrc, cb) {
 /**
  * Run 7za with parameters specified in `paramsArr`.
  * @param {array} paramsArr - array of parameter. Each array item is one parameter.
- * @param {function} cb - callback function. Will be called once command is done. If no errors, first parameter will contain `null`.
+ * @param {function} cb - callback function. Will be called once command is done. If no errors, first parameter will contain `null`. If no output, second parameter will be `null`.
  */
 function cmd(paramsArr, cb) {
     run(path7za, paramsArr, cb);
@@ -44,61 +44,14 @@ function cmd(paramsArr, cb) {
 function run(bin, args, cb) {
     cb = onceify(cb);
     const proc = spawn(bin, args);
-    let output = '';
+    output = '';
     proc.on('error', function (err) {
         cb(err);
     });
     proc.on('exit', function (code) {
-        let result = null;
+        result = null;
         if(args[0] = 'l') {
-            const regex = /(?:(\d{4}-\d{2}-\d{2}) +(\d{2}:\d{2}:\d{2}) +((?:[D.]){1}(?:[R.]){1}(?:[H.]){1}(?:[S.]){1}(?:[A.]){1}) +(\d{1,12}) +(\d{1,12}) +(.+))\n*/g;
-            let m;
-            result = [];
-            while ((m = regex.exec(output)) !== null) {
-                // This is necessary to avoid infinite loops with zero-width matches
-                if (m.index === regex.lastIndex) {
-                    regex.lastIndex++;
-                }
-                
-                let date = "";
-                let time = "";
-                let attr = "";
-                let size = 0;
-                let compressed = 0;
-                let name = "";
-
-                m.forEach((match, groupIndex) => {
-                    switch (groupIndex) {
-                        case 1:
-                            date = match;
-                            break;
-                        case 2:
-                            time = match;
-                            break;
-                        case 3:
-                            attr = match.replace(/\./g,'');
-                            break;
-                        case 4: 
-                            size = match;
-                            break;
-                        case 5:
-                            compressed = match;
-                            break;
-                        case 6:
-                            name = match;
-
-                            result.push({
-                                date: date,
-                                time: time,
-                                attr: attr,
-                                size: size,
-                                compressed: compressed,
-                                name: name,
-                            });
-                            break;
-                    }
-                });
-            }
+            result = parseListCmd(output);
         }
         cb(code ? new Error('Exited with code ' + code) : null, result);
     });
@@ -119,6 +72,58 @@ function onceify(fn) {
     }
 }
 
+function parseListCmd(output) {
+    const regex = /(?:(\d{4}-\d{2}-\d{2}) +(\d{2}:\d{2}:\d{2}) +((?:[D.]){1}(?:[R.]){1}(?:[H.]){1}(?:[S.]){1}(?:[A.]){1}) +(\d{1,12}) +(\d{1,12}) +(.+))\n*/g;
+    result = [];
+    while ((m = regex.exec(output)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+        }
+        
+        date = "";
+        time = "";
+        attr = "";
+        size = 0;
+        compressed = 0;
+        name = "";
+
+        m.forEach((match, groupIndex) => {
+            switch (groupIndex) {
+                case 1:
+                    date = match;
+                    break;
+                case 2:
+                    time = match;
+                    break;
+                case 3:
+                    attr = match.replace(/\./g,'');
+                    break;
+                case 4: 
+                    size = match;
+                    break;
+                case 5:
+                    compressed = match;
+                    break;
+                case 6:
+                    name = match;
+
+                    result.push({
+                        date: date,
+                        time: time,
+                        attr: attr,
+                        size: size,
+                        compressed: compressed,
+                        name: name,
+                    });
+                    break;
+            }
+        });
+    }
+    return result;
+}
+
 exports.unpack = unpack;
 exports.pack = pack;
+exports.list = list;
 exports.cmd = cmd;
