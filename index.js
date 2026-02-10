21283f409d24d82a4a966248b2da79387f2f89fb
Mykhailo Onikiienko <michael.onikienko@gmail.com>
2026-02-10 15:11:28 -0800

    refactor: modernize imports

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const promisify = require('util').promisify;
-const spawn = require('child_process').spawn;
-const path7za = require('7zip-bin').path7za;
+const { promisify } = require('util');
+const { spawn } = require('child_process');
+const { path7za } = require('7zip-bin');
 
 
 /**
